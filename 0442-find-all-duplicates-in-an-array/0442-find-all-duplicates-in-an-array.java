0a78a1c69cb2bcde6ef7643a359332af87699e45
Shyam Sunder Sharma <shyam02072005@gmail.com>
2026-07-30 07:09:44 +0530

    Time: 38 ms (5.61%), Space: 72.8 MB (6.93%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -6,9 +6,7 @@ class Solution {
         for(int num:nums){
             hm.put(num, hm.getOrDefault(num, 0)+1);
             if(hm.get(num)>1){
-                if(!l.contains(num)){
-                    l.add(num);
-                }
+                l.add(num);
             }
         }
         return l;
